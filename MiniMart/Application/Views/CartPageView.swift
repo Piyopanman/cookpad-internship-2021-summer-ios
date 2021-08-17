a190ec98025ba6c3ac2cac71d2006bfe3fa30c8b
Piyopanman <g19911kh@gm.tsuda.ac.jp>
2021-08-17 17:18:21 +0900

    詳細ページリファクタ

--- MiniMart/Application/Views/CartPageView.swift
+++ MiniMart/Application/Views/CartPageView.swift
@@ -1,9 +1,23 @@
 import SwiftUI
 
 struct CartPageView: View {
+    @EnvironmentObject var cartState: CartState
     var body: some View {
-        Text("Hello, World!")
-            .navigationTitle("カート")
+//        VStack{
+//            List(cartState.cartItems, id: \.product.id){ item in
+//                HStack(alignment: .top){
+//                    RemoteImage(urlString: item.product.imageUrl)
+//                                           .frame(width: 100, height: 100)
+//                       VStack(alignment: .leading, spacing: 8) {
+//                           Text(item.product.name)
+//                           Text("\(item.product.price)円")
+//                           Text("\(item.quantity)個")
+//                       }
+//                       .padding(.vertical, 8)
+//                }
+//            }
+//        }
+        Text("hello woidl")
     }
 }
 
@@ -12,5 +26,6 @@ struct CartPageView_Previews: PreviewProvider {
         NavigationView {
             CartPageView()
         }
+        .environmentObject(CartState())
     }
 }
